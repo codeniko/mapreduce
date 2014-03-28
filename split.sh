40aebb0ce7767cc2158aff2c656f73eda0f0937a
Nikolay Feldman <nf139@cs.rutgers.edu>
2014-03-27 23:53:57 -0400

    fixed split if you specify more partitions than lines in file

--- split.sh
+++ split.sh
@@ -14,13 +14,18 @@ perfile=`/usr/bin/expr ${total} / ${2}`
 start=1
 part=0
 
+/usr/bin/touch "${1}.0" # Always have atleast a .0 partition, if empty
 while [ $start -le $total ]; do 
 	end=`/usr/bin/expr ${start} + ${perfile}`
 	end=`/usr/bin/expr ${end} - 1`
 	if [ ${end} -gt ${total} ]; then end=${total}; fi
 	if [ ${part} -eq ${maxpart} ]; then end=${total}; fi
-#	/bin/echo "sed -n \"${start},${end}p\" '<'${1} '>'${1}.${part}"
-	/bin/sed -n "${start},${end}p" <${1} >${1}.${part}
+	# /bin/echo "sed -n \"${start},${end}p\" '<'${1} '>'${1}.${part}"
+	if [ ${end} -eq 0 ]; then
+		/usr/bin/touch "${1}.${part}"
+	else
+		/bin/sed -n "${start},${end}p" <${1} >${1}.${part}
+	fi
 	part=`/usr/bin/expr ${part} + 1`
 	start=`/usr/bin/expr ${end} + 1`
 done
